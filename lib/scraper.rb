d4847e6f03d34a2852888667843c6568523196a1
Tanner Townsend <rsmcwow@gmail.com>
2020-04-13 15:46:41 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -19,7 +19,22 @@ class Scraper
   end
 
   def self.scrape_profile_page(profile_url)
+    student_info = {}
+    html = open(index_url)
+    profile = Nokogirl::HTML(html)
 
+    profile.css("div.main-wrapper.profile .social-icon-container a").each do |social|
+      if social.attribute("href").value.include?("twitter")
+        student_profile[:twitter] = social.attribute("href").value
+      elsif social.attribute("href").value.include?("linkedin")
+        student_profile[:linkedin] = social.attribute("href").value
+      elsif social.attribute("href").value.include?("github")
+        student_profile[:github] = social.attribute("href").value
+      else
+        student_profile[:blog] = social.attribute("href").value
+      end
+    end
+
+    
   end
-
 end
